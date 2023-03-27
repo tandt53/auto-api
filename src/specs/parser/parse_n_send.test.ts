ff6dca84ffbd0e15a7ee22e3194b93c1be9ec80e
tan <dothetan.040490@gmail.com>
2023-03-27 21:31:11 +0700

    update parser and test

---
+++ src/specs/parser/parse_n_send.test.ts
@@ -0,0 +1,31 @@
+import {parseSwaggerJson} from "../../parser/v2/parser";
+import {ApiSpec, request} from "../../httpclient";
+import {expect} from "chai";
+import {ApiConfig} from "../../httpclient/v2/ApiConfig";
+
+describe('POST /user', async () => {
+    const method = 'POST';
+    const path = '/user';
+    const config: ApiConfig = {
+        baseUrl: "https://petstore3.swagger.io/api/v3"
+    }
+    it('should parse and send request successfully', async function () {
+        const apiSpecs: ApiSpec[] = await parseSwaggerJson('openapi.json');
+        // console.log(JSON.stringify(apiSpecs, null, 2));
+        const spec = apiSpecs.find((apiSpec) => apiSpec.method === method && apiSpec.path === path);
+        console.log(JSON.stringify(spec, null, 2));
+        console.log('=========================================');
+
+        const response = await request(spec, config);
+
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.property('id');
+        expect(response.body).to.have.property('username');
+        expect(response.body).to.have.property('firstName');
+        expect(response.body).to.have.property('lastName');
+        expect(response.body).to.have.property('email');
+        expect(response.body).to.have.property('password');
+        expect(response.body).to.have.property('phone');
+        expect(response.body).to.have.property('userStatus');
+    });
+})
